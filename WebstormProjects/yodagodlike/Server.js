8f44bcd7fc204adb9fc49665159cd82d61862a14
Mads505m <onlinebigchungus@gmail.com>
2024-10-21 23:33:05 +0200

    html link

--- WebstormProjects/yodagodlike/Server.js
+++ WebstormProjects/yodagodlike/Server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const characterRoutes = require('./Routes/CharacterRoutes');
 const logRequest = require('./Middleware/LogRequest');
 const UnexceptedError = require('./Middleware/UnexceptedError');
-
+const Homeroutes = require('./Routes/Homeroutes');
 require('dotenv').config({ path: '.env' });
 const PORT = process.env.PORT || 3000;
 
@@ -19,14 +19,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'Public')));
 
 app.use('/', characterRoutes);
+app.use('/', Homeroutes);
 
 
 app.use(UnexceptedError)
 
 
-app.get('/', (req, res) => {
-    res.send('Custom logging with Express');
-});
 
 
 app.listen(PORT, () => {
